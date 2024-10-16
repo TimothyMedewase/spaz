1a0d816184f8f459dee15b7e94484ba619ba7e38
Timothy Medewase <medewaset@gmail.com>
2024-10-16 17:11:56 -0500

    fix auth isssues

--- src/app/actions/SignOutAction.ts
+++ src/app/actions/SignOutAction.ts
@@ -2,7 +2,6 @@
 "use server";
 
 import { signOut } from "@/auth";
-import { redirect } from "next/navigation";
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/login" });
